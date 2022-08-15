4930629fbcab7921cd530ee4e4cff5739865bbe8
Brent <brenton.crowley@gmail.com>
2022-08-15 15:56:57 +1000

    Initial Commit

---
+++ CarListingApp/CarListingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarListingAppApp.swift
+//  CarListingApp
+//
+//  Created by Brent on 15/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarListingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarListingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarListingApp
+//
+//  Created by Brent on 15/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
